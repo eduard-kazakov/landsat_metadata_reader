f13bfa2ea2db70c70474c680d8342e0fc3df1e02
Eduard Kazakov <ee.kazakov@gmail.com>
2019-07-04 18:45:09 +0300

    solar irradiances added

--- landsat_metadata_reader.py
+++ landsat_metadata_reader.py
@@ -6,6 +6,40 @@ class LandsatMetadataReader():
         self.metadata = {}
         self.bands = {}
 
+        solar_irradiances = {'LANDSAT_4':
+                                 {'1':1957,
+                                  '2':1825,
+                                  '3':1557,
+                                  '4':1033,
+                                  '5':214.9,
+                                  '7':80.72},
+                             'LANDSAT_5':
+                                 {'1': 1957,
+                                  '2': 1826,
+                                  '3': 1554,
+                                  '4': 1036,
+                                  '5': 215.0,
+                                  '7': 80.67},
+                             'LANDSAT_7':
+                                 {'1': 1970,
+                                  '2': 1842,
+                                  '3': 1547,
+                                  '4': 1044,
+                                  '5': 225.7,
+                                  '7': 82.06,
+                                  '8': 1369},
+                             'LANDSAT_8':
+                                 {'1': 1895.33,
+                                  '2': 2004.57,
+                                  '3': 1820.75,
+                                  '4': 1549.49,
+                                  '5': 951.76,
+                                  '6': 366.97,
+                                  '7': 247.55,
+                                  '8': 85.46,
+                                  '9': 1723.88}
+                             }
+
         for line in self.metadata_file.readlines():
             if (line.find('GROUP') >= 0) or (line.find('=') == -1):
                 continue
@@ -17,8 +51,9 @@ class LandsatMetadataReader():
         if not 'SPACECRAFT_ID' in self.metadata:
             raise KeyError('Invalid metadata file')
 
-
         all_bands = []
+        reflectance_bands = []
+        thermal_bands = []
 
         if self.metadata['SPACECRAFT_ID'] == 'LANDSAT_8':
             all_bands = [1,2,3,4,5,6,7,8,9,10,11]
@@ -62,14 +97,20 @@ class LandsatMetadataReader():
                 self.bands[str(band)]['reflectance_mult'] = float(self.metadata['REFLECTANCE_MULT_BAND_%s' % band])
                 self.bands[str(band)]['reflectance_add'] = float(self.metadata['REFLECTANCE_ADD_BAND_%s' % band])
 
+                self.bands[str(band)]['solar_irradiance'] = solar_irradiances[self.metadata['SPACECRAFT_ID']][str(band)]
+
+                self.bands[str(band)]['type'] = 'reflectance'
+
             if band in thermal_bands:
                 self.bands[str(band)]['k1_constant'] = float(self.metadata['K1_CONSTANT_BAND_%s' % band])
                 self.bands[str(band)]['k2_constant'] = float(self.metadata['K2_CONSTANT_BAND_%s' % band])
+
+                self.bands[str(band)]['type'] = 'thermal'
 
     def get_band_metadata_by_file_name(self, file_name):
         for band in self.bands.keys():
             if file_name == self.bands[band]['file_name']:
                 return self.bands[band]
 
-#metadata = LandsatMetadataReader('samples/LC08_L1TP_179018_20190606_20190619_01_T1/LC08_L1TP_179018_20190606_20190619_01_T1_MTL.txt')
-#print metadata.get_band_metadata_by_file_name('LC08_L1TP_179018_20190606_20190619_01_T1_B11.TIF')
+metadata = LandsatMetadataReader('samples/LC08_L1TP_179018_20190606_20190619_01_T1/LC08_L1TP_179018_20190606_20190619_01_T1_MTL.txt')
+print metadata.get_band_metadata_by_file_name('LC08_L1TP_179018_20190606_20190619_01_T1_B11.TIF')
